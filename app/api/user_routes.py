89b80307f41c74f5d8c4447518efc8634663b13c
J0914 <jordynsechrist@gmail.com>
2021-07-28 13:12:15 -0500

    complete notebook api routes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -23,34 +23,24 @@ def user(id):
 
 # <<<<< User Notebooks >>>>>
 
-def get_all_notebooks(userId):
-    return Notebook.query.where(Notebook.userId == userId).all()
-
-# get all notebooks or post a new one
-@user_routes.route('/<int:userId>/notebooks', methods=['GET', 'POST'])
+# create a new notebook
+@user_routes.route('/<int:userId>/notebooks', methods=['POST'])
 # @login_required
-def get_and_post_notebooks(userId):
-    if request.method == 'GET':
-        notebooks = get_all_notebooks(userId)
-        return {'notebooks': [notebook.to_dict() for notebook in notebooks]}
-    elif request.method == 'POST':
-        form = NotebookForm()
-        form['csrf_token'].data = request.cookies['csrf_token']
-        if form.validate_on_submit():
-            print('validated!!!!!!!!!!!!!!!!!!!')
-            data = request.get_json()
-            print('THE DATA!!!!!!!!!!!!!!!!!!!', data)
-            notebook = Notebook(userId=userId, title=data['title'])
-            db.session.add(notebook)
-            db.session.commit()
-            notebooks = get_all_notebooks(userId)
-            return {'notebooks': [notebook.to_dict() for notebook in notebooks]}
-        else: 
-            return jsonify({'errors': form.errors})
-    else:
-        raise ValueError('Invalid request method, try a different route')
+def post_notebooks(userId):
+    form = NotebookForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        data = request.get_json()
+        notebook = Notebook(userId=userId, title=data['title'])
+        db.session.add(notebook)
+        db.session.commit()
+        user = User.query.get(userId)
+        return user.to_dict()
+    else: 
+        return jsonify({'errors': form.errors})
 
-# Edit or Delete a notebook
+
+# edit or delete a notebook
 @user_routes.route('/<int:userId>/notebooks/<int:notebookId>', methods=['PATCH', 'DELETE'])
 # @login_required
 def patch_and_delete_notebooks(userId, notebookId):
@@ -62,17 +52,17 @@ def patch_and_delete_notebooks(userId, notebookId):
             notebook = Notebook.query.get(notebookId)
             notebook.title = data['title']
             db.session.commit()
-            notebooks = get_all_notebooks(userId)
-            return {'notebooks': [notebook.to_dict() for notebook in notebooks]}
+            user = User.query.get(userId)
+            return user.to_dict()
+        else:
+            return jsonify({'errors': form.errors})
     elif request.method == 'DELETE':
         notebook = Notebook.query.get(notebookId)
         db.session.delete(notebook)
         db.session.commit()
-        notebooks = get_all_notebooks(userId)
-        return {'notebooks': [notebook.to_dict() for notebook in notebooks]}
+        user = User.query.get(userId)
+        return user.to_dict()
     else:
         raise ValueError('Invalid request method, try a different route')
-
-    
 
 
