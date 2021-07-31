7c25ca6a298016148100036e38de4875e0cd2da2
J0914 <jordynsechrist@gmail.com>
2021-07-31 14:36:59 -0500

    functionality updates to notebooks dropdown

---
+++ react-app/src/components/NotePage/NoteForm/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import * as notebookActions from '../../../store/notebooks'
+import {BsCheck, BsX} from 'react-icons/bs'
+
+import styles from '../../css-modules/noteform.module.css';
+
+const NoteForm = ({setShowNotebookForm}) => {
+    const [title, setTitle] = useState('')
+    const [errors, setErrors] = useState([])
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.session.user)
+
+    const createNote = async (e) => {
+        e.preventDefault();
+        let userId;
+        if (user) userId = user.id
+        const data = await dispatch(notebookActions.createNotebook(userId, title));
+        if (data) {
+          setErrors(data);
+        } else {
+            setShowNotebookForm(false)
+        }
+      };
+
+    return (
+        // <form id={styles.notebook_form} onSubmit={createNotebook}>
+        //         {errors.map((error, ind) => (
+        //         <li className={styles.error} key={ind}>{error}</li>
+        //         ))}
+        //         <input 
+        //         type="text" 
+        //         placeholder="Notebook Name" 
+        //         value={title}
+        //         onChange={(e) => setTitle(e.target.value)}
+        //         className={styles.notebook_name}
+        //         required={true}
+        //         />
+        //         <button type="submit" className={styles.notebook_form__submit}><BsCheck /></button>
+        //         <button onClick={() => setShowNotebookForm(false)} className={styles.notebook_form__cancel}><BsX /></button>
+        //     </form>
+    )
+}
+
+export default NoteForm;
--- react-app/src/components/NotebooksDropdownMenu/index.js
+++ react-app/src/components/NotebooksDropdownMenu/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useState, useRef, useEffect } from 'react';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {NavLink} from 'react-router-dom';
-import {BsPlusCircle, BsTrash} from 'react-icons/bs'
-import NotebookForm from '../NotebookForm'
+import {BsPlusCircle, BsDashCircle, BsTrash, BsPencil} from 'react-icons/bs'
+import NotebookForm from './NotebookForm'
+import EditNotebookForm from './EditNotebookForm'
+import * as notebookActions from '../../store/notebooks'
 
 import styles from '../../css-modules/notebookdropdown.module.css'
 
@@ -11,7 +13,10 @@ const NotebooksDropdownMenu = () => {
     const dropdownRef = useRef(null);
     const [isOpen, setIsOpen] = useState(false);
     const [showNotebookForm, setShowNotebookForm] = useState(false);
-  
+    const [showEditNotebookForm, setShowEditNotebookForm] = useState(false);
+    const dispatch = useDispatch();
+
+    const user = useSelector(state => state.session.user)
     const notebooks = useSelector(state => state.notebooks.notebooks);
 
     const handleClick = () => setIsOpen(!isOpen);
@@ -20,8 +25,12 @@ const NotebooksDropdownMenu = () => {
         return;
     }
 
-    const deleteNotebook = () => {
-        return;
+    const deleteNotebook = (notebookId) => {
+        let userId;
+        if (user) {
+            userId = user.id
+        }
+        dispatch(notebookActions.deleteNotebook(userId, notebookId))
     }
 
 
@@ -31,20 +40,26 @@ const NotebooksDropdownMenu = () => {
                 <span>Notebooks</span>
             </button>
             {isOpen && <div ref={dropdownRef} className={`${styles.notebook_menu} ${isOpen ? styles.active : styles.inactive}`}>
-            <button className={styles.add_notebook} onClick={() => setShowNotebookForm(true)}>Create Notebook <BsPlusCircle /></button>
+            <button className={styles.add_notebook} onClick={() => setShowNotebookForm(!showNotebookForm)}>Create Notebook {!showNotebookForm ? <BsPlusCircle /> : <BsDashCircle />}</button>
             {showNotebookForm && <NotebookForm setShowNotebookForm={setShowNotebookForm} />}
                 {notebooks?.map((notebook) => (
                     <div  key={notebook.id}>
                     <div className={styles.notebook_container}>
-                        <ul className={styles.notebook_title}>{notebook.title}</ul>
-                        <button className={styles.delete_notebook} onClick={() => deleteNotebook}><BsTrash /></button>
+                        {!showEditNotebookForm && <ul className={styles.notebook_title}>{notebook.title}</ul>}
+                        {showEditNotebookForm && <EditNotebookForm setShowEditNotebookForm={setShowEditNotebookForm} currentTitle={notebook.title} notebookId={notebook.id}/>}
+                        { !showEditNotebookForm &&
+                        <>
+                        <button className={styles.edit_notebook} onClick={() => setShowEditNotebookForm(true)}><BsPencil /></button>
+                        <button className={styles.delete_notebook} onClick={() => deleteNotebook(notebook.id)}><BsTrash /></button>
+                        </>
+                        }
                     </div>
-                        <button className={styles.add_note} onClick={() => createNote}>Create Note <BsPlusCircle /></button>
+                        <NavLink className={styles.add_note} to={`/notebooks/${notebook.id}/notes`}>Create Note <BsPlusCircle /></NavLink>
                         <div className={styles.notes_container}>
                         {
                             Object.values(notebook.notes).map(note => {
                                 return (
-                                    <NavLink className={styles.note_nav}to={`/notebook/${notebook.id}/notes/${note.id}`} key={note.id}>{note.title}</NavLink>
+                                    <NavLink className={styles.note_nav}to={`/notebooks/${notebook.id}/notes/${note.id}`} key={note.id}>{note.title}</NavLink>
                                 )
                             })
                         }
